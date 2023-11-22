0aa7775fd917317d334c0749593f4b75e4fe899b
unknown <lisandro.batiancila@gmail.com>
2023-11-23 05:45:51 +0800

    admin side continuation

--- src/utils/utilsFunct.ts
+++ src/utils/utilsFunct.ts
@@ -1,11 +1,11 @@
 import moment from "moment";
 
 export const toUpperCase = (name: string) => {
-    let newName = name.split(',');
+    let newName = name.split(' ');
 
     let tempName:string = "";
     for(let i = 0; i < newName.length; i++) {
-        tempName += newName[i].substring(0, 1).toUpperCase()+newName[i].substring(1, newName[i].length);
+        tempName += newName[i].substring(0, 1).toUpperCase()+newName[i].substring(1, newName[i].length)+' ';
     }
 
     return tempName;
